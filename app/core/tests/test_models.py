cf7884b55c20f7aa6ac8d269d4badceaa5e8c621
ukibbb <lucas.bulinski@gmail.com>
2020-01-08 18:01:37 +0100

    added endpoint for creating and authenticating users

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -30,7 +30,7 @@ class ModelTests(TestCase):
 
     def test_create_new_super_user(self):
         """ Test creating a new super user"""
-        user = get_user_model().objects.create_super_user(
+        user = get_user_model().objects.create_superuser(
             'test@lucas.pl',
             'test123'
         )
